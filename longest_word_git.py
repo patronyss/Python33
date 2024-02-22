c0bc092b471d357af09a5a04c1cb8938d8e7cd1b
petro olenych <opv1224@gmail.com>
2024-02-22 22:11:33 +0200

    created files

--- longest_word_git.py
+++
@@ -1,4 +0,0 @@
-with open('C:\\Users\\PC\\Documents\\opv1224\\tech_learn\\itstep\\python_course\\coding\\Python_Basic\\homework\\longestwordgit\\Python33\\input.txt', 'r') as file:
-    content = file.readlines()
-    print(max(content, key=len), end='')
-    
